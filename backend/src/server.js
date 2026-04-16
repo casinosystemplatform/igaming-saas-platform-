af16fe42b98c01680ca92d7154d61c7b388faa62
casinosystemplatform <mrsuparwincasinooffical@gmail.com>
2026-04-17 02:45:10 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,21 @@
+import Tenant from "../models/Tenant.js";
 
+export const tenantMiddleware = async (req, res, next) => {
+  try {
+    const domain = req.headers.host;
+
+    const tenant = await Tenant.findOne({
+      "branding.domain": domain
+    });
+
+    if (!tenant) {
+      return res.status(404).json({ message: "Tenant not found" });
+    }
+
+    req.tenant = tenant;   // attach tenant to request
+    next();
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
