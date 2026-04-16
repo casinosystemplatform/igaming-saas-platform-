2113b47efed9833e999ec2c5e11fc5640a391f2e
casinosystemplatform <mrsuparwincasinooffical@gmail.com>
2026-04-17 02:16:37 +0600

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,1 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import CrashGame from "../games/crash/CrashGame";
 
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/crash" element={<CrashGame />} />
+    </Routes>
+  );
+}
